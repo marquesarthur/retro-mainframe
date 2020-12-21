38170be9cacd323619bfbfc829abecbd00afdec6
Arthur Marques <marques.s.arthur@gmail.com>
2020-12-21 02:53:45 -0800

    added sound effects - still needs boolean to enable/disable sound

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,24 +5,25 @@ var b = undefined;
 
 
 var GLITCH = {
-    destroy : false, // set 'true' to stop the plugin
+    destroy: false, // set 'true' to stop the plugin
     glitch: true, // set 'false' to stop glitching
     scale: true, // set 'false' to stop scaling
-    blend : true, // set 'false' to stop glitch blending
-    blendModeType : 'hue', // select blend mode type
-    glitch1TimeMin : 600, // set min time for glitch 1 elem
-    glitch1TimeMax : 900, // set max time for glitch 1 elem
-    glitch2TimeMin : 10, // set min time for glitch 2 elem
-    glitch2TimeMax : 115, // set max time for glitch 2 elem
-    zIndexStart : 8, // because of absolute position, set z-index base value
+    blend: true, // set 'false' to stop glitch blending
+    blendModeType: 'hue', // select blend mode type
+    glitch1TimeMin: 600, // set min time for glitch 1 elem
+    glitch1TimeMax: 900, // set max time for glitch 1 elem
+    glitch2TimeMin: 10, // set min time for glitch 2 elem
+    glitch2TimeMax: 115, // set max time for glitch 2 elem
+    zIndexStart: 8, // because of absolute position, set z-index base value
 };
 
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_typewriter
 function typeWriter(id, message, i) {
+    $("#printer-audio").get(0).play();
     if (i < message.length) {
-        document.getElementById(id).innerHTML += message.charAt(i);        
+        document.getElementById(id).innerHTML += message.charAt(i);
         i++;
-        setTimeout(function() {
+        setTimeout(function () {
             typeWriter(id, message, i);
         }, TYPEWRITER_SPEED);
     } else {
@@ -46,38 +47,40 @@ function log(message) {
 function createServer() {
     try {
         b = Bugout("bugout-chat-tutorial");
-        b.on("seen", function (address) { 
-            log(address + " [ connected ]"); 
+        b.on("seen", function (address) {
+            log(address + " [ connected ]");
         });
-        
+
         b.on("message", function (address, message) {
             log(address + ": " + message);
         });
 
         return b;
-    } catch(ex) {
+    } catch (ex) {
         console.log(ex);
         log("Error... reconnecting");
-        setTimeout(createServer, 5000);
     }
 }
 
-
-
-
-$(function() {
+// MAIN
+$(function () {
     // Execute a function when the user releases a key on the keyboard
-    $("#input").on("keydown", function(event) {
+    $("#input").on("keydown", function (event) {
         var key = event.key;
         if (key === ENTER) {
-            event.preventDefault();            
-                b.send(event.target.textContent);
-                event.target.textContent = "";
-            
+            event.preventDefault();
+            b.send(event.target.textContent);
+            event.target.textContent = "";
         }
+    });
+
+    $("#input").on("keyup", function (event) {
+        var audio = new Audio('typewriter/typewriter-key-even.wav');
+        audio.play();
     });
 
     var welcomeMessage = "Please wait... establishing connection";
     typeWriter("welcome", welcomeMessage, 0);
+    
     createServer();
 });
